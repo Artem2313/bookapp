53c2d326d2f782db96364a188de3212b9c537da2
Artem2313 <huselnykov@gmail.com>
2020-08-10 19:25:35 +0300

    useState

--- src/SingleBook.js
+++ src/SingleBook.js
@@ -1,14 +1,32 @@
-import React from "react";
-const SingleBook = ({ title, id, finished }) => (
-  <li className="book">
-    <label>
-      <input type="checkbox" defaultChecked={false} />
-      <span>{title}</span>
-      <button type="button" class="waves-effect waves-light btn">
-        Delete
-      </button>
-    </label>
-  </li>
-);
+import React, { useState } from "react";
+//14) добавляем useState
+//15) в зависимости от флага комплитед, отображать в чекбоксе стейт
+const SingleBook = ({ title, id, completed }) => {
+  //16) воспользуемся юз стейт, куда передадим флаг комплитед
+  const [checked, setChecked] = useState(completed);
+  //17) Теперь в зависимости от checked нам нужно будет добавлять или убирать класс с лишки
+  const checking = ["book"];
+
+  //18) проверяем checked данный класс описан в индекс цсс и добавляет line-through
+  if (checked) {
+    checking.push("completed");
+  }
+  return (
+    //19) Добавляем этот класс через джоин и меняем дефолт на checked, добавляем onChange onChange={() => setChecked(!checked)
+    <li className={checking.join(" ")}>
+      <label>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={() => setChecked(!checked)}
+        />
+        <span>{title}</span>
+        <button type="button" class="waves-effect waves-light btn">
+          Delete
+        </button>
+      </label>
+    </li>
+  );
+};
 
 export default SingleBook;
