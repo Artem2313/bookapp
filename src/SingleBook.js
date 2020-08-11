14ce11ef4fb8a28a4ab73dc80678cd131718d8ac
Artem2313 <huselnykov@gmail.com>
2020-08-11 10:02:58 +0300

    start

--- src/SingleBook.js
+++ src/SingleBook.js
@@ -1,26 +1,15 @@
 import React, { useState } from "react";
-//14) добавляем useState
 
 const SingleBook = ({ title, id, completed }) => {
-  //15) в зависимости от флага комплитед, отображать в чекбоксе стейт
   const [checked, setChecked] = useState(completed);
 
-  //16) воспользуемся юз стейт, куда передадим флаг комплитед
-
-  //17) Теперь в зависимости от checked нам нужно будет добавлять или убирать класс с лишки
   const checking = ["book"];
 
-  //18) проверяем checked данный класс описан в индекс цсс и добавляет line-through
   if (checked) {
     checking.push("completed");
   }
 
   return (
-    //19) Добавляем этот класс через джоин и меняем дефолт на checked, добавляем onChange onChange={() => setChecked(!checked)
-    // checked={completed} вместо default
-    //19.1) добавим onChange, куда инвертируем событие setChecked(!checked)}
-    //   onChange={() => setChecked(!checked)}
-    //20) Возвращаемся в Апп, на этом работа с useState закончена
     <li className="book">
       <label>
         <input
