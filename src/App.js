65a5df038708fa17d5918a5d4e0a6378333ff453
Artem2313 <huselnykov@gmail.com>
2020-08-11 09:49:18 +0300

    useState done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import BookList from "./BookList";
 
-export default class App extends Component {
-  state = {
-    books: [
-      { id: 1, title: "Beauty and the Beast" },
-      { id: 2, title: "Cinderella" },
-      { id: 3, title: "The Little Mermaid" },
-    ],
+/*
+1) Заменить Апп на конст Апп, а также убрать наследование от компонента
+2) Удалим компонент и исправим сам Апп
+*/
+
+const App = () => {
+  // 3) Комментим стейт и убираем рендер, а оставляем ретерн с шаблоном jsx
+  // state = {
+  //   books: [
+  //     { id: 1, title: "Beauty and the Beast" },
+  //     { id: 2, title: "Cinderella" },
+  //     { id: 3, title: "The Little Mermaid" },
+  //   ],
+  // };
+
+  // 4) Импортируем первый хук из реакта useState
+  // 5) Usestate позволяет работать в функциональных компонентах со стейтом. Результат функции Usestate это массив из двух элементов
+  // где первый элемент это само начальное состояние, а второй элемент - это функция для изменения состояния
+  // передаем начальное состояние books, как было в стейте
+
+  // 6) Передаем books в компонент без this.state. Если посмотреть в браузер, то состояние не изменилось,
+  //но мы теперь юзаем только функц. компонент
+
+  const [books, setBooks] = useState([
+    { id: 1, title: "Beauty and the Beast" },
+    { id: 2, title: "Cinderella" },
+    { id: 3, title: "The Little Mermaid" },
+  ]);
+
+  // 7) Сейчас мы добавим новый UseState для тайтла чтобы обработать инпут
+  const [bookTitle, setBookTitle] = useState("");
+
+  // 9) Реализовываем функц addBook
+  const addBook = (event) => {
+    //10) Проверим нажимали ли мы Энтер
+    if (event.key === "Enter") {
+      //11) Реализовываем добавление setBooks, спредаем старый массив и добавляем новую книгу
+      setBooks([
+        ...books,
+        { id: Date.now(), title: bookTitle, completed: false },
+      ]);
+      //12) Для очистки тайтла - вызываем сет стейт с пустой строкой
+      setBookTitle("");
+      //13) Для того, чтобы отрисовать прочтение или непрочтение книги - перейдем в SingleBook
+    }
   };
-  render() {
-    return (
-      <div className="container">
-        <h1>Bookapp</h1>
-        <div className="input-field">
-          <input type="text" />
-          <label className="active">Book's Title</label>
-        </div>
-        <BookList books={this.state.books} />
+
+  return (
+    <div className="container">
+      <h1>Bookapp</h1>
+      <div className="input-field">
+        {/* 7.1) Свяжем модель тайтла и инпута добавив value, а также добавим onKeyPress  случшатель, чтобы добавлять новый book по enter в массив
+            8) onKeyPress реализуем как стрелочную функцию в Апп addBook    
+            value={bookTitle}
+            onChange={(event) => setBookTitle(event.target.value)}
+            onKeyPress={addBook} 
+        */}
+        <input
+          type="text"
+          value={bookTitle}
+          onChange={(event) => setBookTitle(event.target.value)}
+          onKeyPress={addBook}
+        />
+        <label className="active">Book's Title</label>
       </div>
-    );
-  }
-}
+      {/* {передаем books вместо this.state.books} */}
+      <BookList books={books} />
+    </div>
+  );
+};
+
+export default App;
--- src/SingleBook.js
+++ src/SingleBook.js
@@ -1,14 +1,40 @@
-import React from "react";
-const SingleBook = ({ title, id, finished }) => (
-  <li className="book">
-    <label>
-      <input type="checkbox" defaultChecked={false} />
-      <span>{title}</span>
-      <button type="button" className="waves-effect waves-light btn">
-        Delete
-      </button>
-    </label>
-  </li>
-);
+import React, { useState } from "react";
+//14) добавляем useState
+
+const SingleBook = ({ title, id, completed }) => {
+  //15) в зависимости от флага комплитед, отображать в чекбоксе стейт
+  const [checked, setChecked] = useState(completed);
+
+  //16) воспользуемся юз стейт, куда передадим флаг комплитед
+
+  //17) Теперь в зависимости от checked нам нужно будет добавлять или убирать класс с лишки
+  const checking = ["book"];
+
+  //18) проверяем checked данный класс описан в индекс цсс и добавляет line-through
+  if (checked) {
+    checking.push("completed");
+  }
+
+  return (
+    //19) Добавляем этот класс через джоин и меняем дефолт на checked, добавляем onChange onChange={() => setChecked(!checked)
+    // checked={completed} вместо default
+    //19.1) добавим onChange, куда инвертируем событие setChecked(!checked)}
+    //   onChange={() => setChecked(!checked)}
+    //20) Возвращаемся в Апп, на этом работа с useState закончена
+    <li className="book">
+      <label>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={() => setChecked(!checked)}
+        />
+        <span>{title}</span>
+        <button type="button" className="waves-effect waves-light btn">
+          Delete
+        </button>
+      </label>
+    </li>
+  );
+};
 
 export default SingleBook;
