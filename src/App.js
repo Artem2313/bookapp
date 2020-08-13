84cced5e1798908b84e384ca5f6366d12380c1b4
Artem2313 <huselnykov@gmail.com>
2020-08-13 10:16:33 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
 import React, { useState, useEffect } from "react";
-//21.1) { useState, useEffect } from "react";
 import BookList from "./BookList";
 
+// 27) Передаем контекст
+import { Context } from "./Context";
+
 const App = () => {
-  const [books, setBooks] = useState([
-    //21) Поработаем немного с UseEffect, а именно с localstorage - уберем локальный стейт
-    // { id: 1, title: "Beauty and the Beast" },
-    // { id: 2, title: "Cinderella" },
-    // { id: 3, title: "The Little Mermaid" },
-  ]);
+  const [books, setBooks] = useState([]);
 
   const [bookTitle, setBookTitle] = useState("");
-
-  // 24) Добавим еще один UseEffect для отображения при перегрузке элементов, он будет срабатывать один раз при загрузке
-  // Заберем от туда books или вернем пустой массив в стейт books
 
   useEffect(() => {
     const arr = localStorage.getItem("books") || [];
     setBooks(JSON.parse(arr));
   }, []);
 
-  // 22) UseEffect (добавим как реакт компонент), благодаря этому хуку - мы можем реализовывать life cycle хуки в функциональных компонентах
-  // Он принимает в себя в первый аргумент callback, который будет выполняться, а вторым параметром передается список зависимостей, на
-  // которые откликается UseEffect
-  // добавим консоль лог, чтобы посмотреть в целом, когда он меняется, а меняться он будет на каждое изменение от ввода в тайтл, до добавления книги
-  useEffect(
-    () => {
-      // console.log("init");
-      //23) зададим локал сторадж с зависимостью массива books и обернем его в джейсон
-      // но стоит учесть, что сейчас в локал сторидж ничего не сохраниться при перегрузке
-      localStorage.setItem("books", JSON.stringify(books));
-      //для начала консоль логнем и проверим, когда вызывается? По-факту, он реализовывается на каждое изменение состояния
-    },
-    //22.1) Добавим зависимость, теперь инит вызовется только при загрузке, т.к. нет зависимостей, таким образом
-    // делается эмуляция componentDidMount
-    // Теперь добавим зависимость, к примеру, пустой массив. Так мы сможем сэмулировать componentDidMount. Тоесть, у нас будет один консоль лог, который не повторяется
-    // Если в массив зависимостей мы передадим какую-то часть стейта, то на каждое ее изменение будет вызываться и UseEffect
-    // сейчас добавим books, т.к. мы будем работать с ними в локал сторидж в зависимости, и посмотрим, что будет выводить консоль лог
-
-    [books]
-  );
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
 
   const addBook = (event) => {
     if (event.key === "Enter") {
@@ -52,21 +29,50 @@ const App = () => {
     }
   };
 
-  return (
-    <div className="container">
-      <h1>Bookapp</h1>
-      <div className="input-field">
-        <input
-          type="text"
-          value={bookTitle}
-          onChange={(event) => setBookTitle(event.target.value)}
-          onKeyPress={addBook}
-        />
-        <label className="active">Book's Title</label>
-      </div>
+  // 30) Создадим две функции, одна в ответе за удаление книги
+  const delBook = (id) => {
+    setBooks(
+      books.filter((book) => {
+        return book.id !== id;
+      })
+    );
+  };
+  //31) вторая в ответе за состояние checked
 
-      <BookList books={books} />
-    </div>
+  const toggleCheck = (id) => {
+    setBooks(
+      books.map((book) => {
+        if (book.id === id) {
+          book.completed = !book.completed;
+        }
+        return book;
+      })
+    );
+  };
+
+  //32) передаем эти две функции в провайдер Context.Provider value={{ toggleCheck, delBook }}
+  //33) перейдем в SingleBook, чтобы получить эти функции
+
+  return (
+    // 28) Оборачиваем весь шаблон в провайдер контекста <Context.Provider>
+    //29) передаем значение value, которые будут содержать в себе функции для изменения главного стейт value={{}}
+    //29.1) Получаем контекст в SingleBook,будущие функции тоггле и удаления
+
+    <Context.Provider value={{ toggleCheck, delBook }}>
+      <div className="container">
+        <h1>Bookapp</h1>
+        <div className="input-field">
+          <input
+            type="text"
+            value={bookTitle}
+            onChange={(event) => setBookTitle(event.target.value)}
+            onKeyPress={addBook}
+          />
+          <label className="active">Book's Title</label>
+        </div>
+        <BookList books={books} />
+      </div>
+    </Context.Provider>
   );
 };
 
--- src/SingleBook.js
+++ src/SingleBook.js
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
+//34) Убираем useState, больше он нам в этом компоненте не нужен
+//35) Добавляем контекст и useContext
+import { Context } from "./Context";
 
 const SingleBook = ({ title, id, completed }) => {
-  const [checked, setChecked] = useState(completed);
+  //40) Удаляем useState
+  //   const [checked, setChecked] = useState(completed);
+
+  //36) Добавляем useContext с объектом контекста которого мы хотим определить, в объекте те функции, которые мы передавали
+  const { toggleCheck, delBook } = useContext(Context);
 
   const checking = ["book"];
 
-  if (checked) {
+  // if (checked) {
+  //   checking.push("completed");
+  // }
+
+  //39) локальный стейт нам больше не нужен, поэтому мы заменим его, а будем брать флажек напрямую от родителя, в шаблоне тоже меняем checked на completed
+  // Таким образом, у нас все сохраняется в локале, и мы можем все удалять
+  if (completed) {
     checking.push("completed");
   }
 
@@ -14,11 +27,19 @@ const SingleBook = ({ title, id, completed }) => {
       <label>
         <input
           type="checkbox"
-          checked={checked}
-          onChange={() => setChecked(!checked)}
+          checked={completed}
+          // checked={checked}
+          //   onChange={() => setChecked(!checked)}
+          //37) вызовем функцию в onChange
+          onChange={() => toggleCheck(id)}
         />
         <span>{title}</span>
-        <button type="button" className="waves-effect waves-light btn">
+        <button
+          type="button"
+          className="waves-effect waves-light btn"
+          //38) добавим удаление
+          onClick={() => delBook(id)}
+        >
           Delete
         </button>
       </label>
